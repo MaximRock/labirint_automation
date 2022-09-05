0a91d345090a35670f64889600d709c13841cf04
MaximRock <95434302+MaximRock@users.noreply.github.com>
2022-09-05 16:15:22 +0300

    Update 05.09.2022 adding comments to a file settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 my_login = 'testrock75@gmail.com'
-my_code = '0CC2-4F28-831B'
+my_code = '7546-4A0C-AE8E'
 my_cabinet = 'Личный кабинет'
 captain_daughter = 'Капитанская дочка'
 
@@ -15,39 +15,45 @@ def generate_string(n):
 def russian_chars():
     return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
+
+"""Для теста авторизации"""
 list_discount_phone_mail_negative = [
     '', 'testrock75gmail.com', '@gmail.com', 'testrock75@gmail com', 'максим@gmail.com', -1, 0, '+79995', '+79995621',
     '+7999562154', '7544A0C-AE8E', generate_string(1001), russian_chars(), russian_chars().upper(), special_chars()
 ]
 
-
+"""Для теста авторизации"""
 list_my_discount_code_negative = [
     '', -1, 0, '7544A0C-AE8E', generate_string(1001), russian_chars().upper(), special_chars()
 ]
 
+"""Для теста авторизации"""
 list_discount_phone_mail_positive = [
     '+79291295001', my_login, my_code
 ]
 
+"""Для теста авторизации"""
 list_social_elements = [
     'ВКонтакте', 'ОК', '@mail', 'Яндекс', 'Google'
 ]
 
+"""Для теста личного кабинета в header"""
 list_header_personal = [
     'Сообщения', 'Мой лабиринт', 'Отложено', 'Корзина'
 ]
 
+"""Для теста личного кабинета в header ссылка Мой лабиринт"""
 list_header_personal_button_my_maze_dropdown_menu = [
     'Заказы', 'вы смотрели', 'отложенные', 'баланс', 'настройки', 'выход'
 ]
 
+"""Для теста поля Поиск, вводимое значение Капитанская дочка"""
 list_of_values_in_the_search_field = [
     'Капитанская Дочка', 'капитанская дочка', 'КАПИТАНСКАЯ ДОЧКА', 'КаПиТаНсКаЯ дОчКа', 'дочка капитанская',
     'rfgbnfycrfz ljxrf', 'каитанская дка', '  капитанская дочка', 'капитанская дочка  ',
     'капитанская    дочка', '|\\/!@#$%^&*()-_=+`~?"№;:[]{}капитанская дочка',
     'капитанская|\\/!@#$%^&*()-_=+`~?"№;:[]{} дочка', 'капитанская дочка|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
 ]
-
 
 # idis_list_discount_phone_mail_negative = [
 #     'empty_value', 'email_no_name', 'email_no_dot', 'email_cyrillic', 'negative_number', 'zero', 'phone_5_characters',
